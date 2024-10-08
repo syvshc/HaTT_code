3ce0736789e27b140e0dcaa2f51f16c86f12a786
syvshc <syvshc@foxmail.com>
2024-10-08 11:15:13 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+# README
+
+This repository contains the code of [2410.04385](https://arxiv.org/abs/2410.04385)
+
+The `daas` directory includes the restructured code for algorithms from [Randomized Algorithms for Rounding in the Tensor-Train Format](https://epubs.siam.org/doi/10.1137/21M1451191), utilizing data structures from the [TT-Toolbox](https://github.com/oseledets/TT-Toolbox).
+
+The `hadamard` and `test` directories contain the implementation of algorithms from our paper, along with the numerical experiences' codes in Section 4.
--- rebuttal/test_W_rank.m
+++ rebuttal/test_W_rank.m
@@ -1,24 +1,50 @@
 %% This is a maniscript for testing if W has low-rank structure
 clear
 
-item_num = 60;
-d = 7;
-n = 10;
-len = n^d;
-t1 = 2*pi/len:2*pi/len:2*pi;
-a = 10 * rand(1, item_num) + 0.1;
-b = 10 * rand(1, item_num) + 0.1;
-y1 = 0; z1 = 0;
-for k = 1: item_num
-  y1 = y1 + a(k) * sin(k * t1);
-  z1 = z1 + b(k) * cos(k * t1);
-end
-y = y1; z = z1;
+n = 20 * ones(7, 1);
+d = length(n);
+r = [1; 100 * ones(d - 1, 1); 1];
 
-sz = n * ones(1, d);
-y = tt_tensor(reshape(y, sz))
-z = tt_tensor(reshape(z, sz))
-clear y1 z1 t1 a b;
+%% Set cores
+
+y = tt_tensor();
+y.n = n; y.d = d; y.r = r;
+y.ps = cumsum([1; r(1 : end-1) .* n .* r(2 : end)]);
+y.core = zeros(y.ps(end)-1, 1);
+for k = 1 : d
+  core = zeros(r(k), n(k), r(k + 1));
+  for i1 = 1 : r(k)
+    for i2 = 1 : n(k)
+      for i3 = 1 : r(k + 1)
+        core(i1, i2, i3) = 1 / (i1 + i2 + i3 - 1);
+      end
+    end
+  end
+  y.core(y.ps(k) : y.ps(k + 1) - 1) = core(:);
+end
+z = y;
+%%
+% item_num = 60;
+% d = 7;
+% n = 10;
+% len = n^d;
+% t1 = 2*pi/len:2*pi/len:2*pi;
+% a = 10 * rand(1, item_num) + 0.1;
+% b = 10 * rand(1, item_num) + 0.1;
+% y1 = 0; z1 = 0;
+% for k = 1: item_num
+%   y1 = y1 + a(k) * sin(k * t1);
+%   z1 = z1 + b(k) * cos(k * t1);
+% end
+% y = y1; z = z1;
+
+% n = n * ones(1, d);
+% y = tt_tensor(reshape(y, n))
+% z = tt_tensor(reshape(z, n))
+% clear y1 z1 t1 a b;
+
+%%
+
 % ranks to test every 2
 test_ranks = [20, 40, 60];
 
@@ -30,7 +56,7 @@ f = figure('Name','testWsv');
 f.Position = [46.6,199.4,1404.8,416];
 for i = 1 : L
     ell = test_ranks(i);
-    R = TTrandn(sz, ell);
+    R = TTrandn(n, ell);
     l = R.r;
     W_trunc = HPCRL1(y, z, R);
     for k = 1 : d - 1
@@ -39,8 +65,9 @@ for i = 1 : L
       if k == 2
           [U, S, V] = svd(W1, 'econ'); 
           subplot(1, 3, i);
-          axis square;
           semilogy(diag(S));
+          axis square;
+          ylim([1e-20, 1])
           title(['target rank = ', num2str(ell)])
       end
     end
